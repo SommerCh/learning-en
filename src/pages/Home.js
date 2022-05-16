2ba9dfe519e465256db422dfb974e7895eeab609
SommerCh <47107441+SommerCh@users.noreply.github.com>
2022-05-16 08:21:36 +0200

    img

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,36 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import ImagesOutput from "../components/ImageGrid/ImagesOutput";
+import Klasse1 from "../assets/klasse.jpg";
+import Klasse2 from "../assets/klasse2.jpg";
+import Klasse3 from "../assets/klasse3.jpg";
+
 
 export default function Home() {
   gsap.registerPlugin(ScrollTrigger);
 
   return (
     <div className="home">
-      <h1>Home</h1>
-      <ImagesOutput />
+      <h1>Fremtidens møbler til nutidens børn</h1>
 
-      <div className="box2"></div>
-      <div className="box3"></div>
+      <div className="flex">
+
+     
+        <div className="classroom">
+        <h2>1950</h2>
+          <img src={Klasse3} alt="classroom" />
+        </div>
+
+        <div className="classroom">
+        <h2>1970</h2>
+          <img src={Klasse1} alt="classroom" />
+        </div>
+
+        <div className="classroom">
+          <h2>2020</h2>
+          <img src={Klasse2} alt="classroom" />
+        </div>
+
+      </div>
     </div>
   );
 }
