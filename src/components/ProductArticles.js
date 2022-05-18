825c804e07f8e89f6e8c8d230fa54fddd63fe826
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-05-18 15:44:16 +0200

    Added zone badges to articles

--- src/components/ProductArticles.js
+++ src/components/ProductArticles.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Tooltip from "./ToolTip";
 
 export default function ProductArticles() {
     const [products, setProducts] = useState([]);
@@ -15,12 +16,66 @@ export default function ProductArticles() {
         getData();
     }, [])
 
-    // Fetch procudt imgs
+    // Fetch product imgs
     function getImg(product) {
         if (product.Files?.length >= 1) {
             return product.Files[0]?.Uri;
         } 
     }
+
+    // Fetch product zones + add tooltips
+    function getZones(product) {
+        if (product.Zones?.length === 1) {
+            return (
+                <Tooltip text={product.Zones[0]?.Name}>
+                    <img src={product.Zones[0]?.ImgUrl} alt={product.Zones[0]?.Name} dataTooltip={product.Zones[0]?.Name}/>
+                </Tooltip>
+                );
+        } if (product.Zones?.length === 2) {
+            return (
+                <>
+                <Tooltip text={product.Zones[0]?.Name}>
+                    <img src={product.Zones[0]?.ImgUrl} alt={product.Zones[0]?.Name} dataTooltip={product.Zones[0]?.Name}/>
+                </Tooltip>
+                <Tooltip text={product.Zones[1]?.Name}>
+                    <img src={product.Zones[1]?.ImgUrl} alt={product.Zones[1]?.Name} dataTooltip={product.Zones[1]?.Name}/>
+                </Tooltip>
+                </>
+            );
+        } if (product.Zones?.length === 3) {
+            return (
+                <>
+                <Tooltip text={product.Zones[0]?.Name}>
+                    <img src={product.Zones[0]?.ImgUrl} alt={product.Zones[0]?.Name} dataTooltip={product.Zones[0]?.Name}/>
+                </Tooltip>
+                <Tooltip text={product.Zones[1]?.Name}>
+                    <img src={product.Zones[1]?.ImgUrl} alt={product.Zones[1]?.Name} dataTooltip={product.Zones[1]?.Name}/>
+                </Tooltip>
+                <Tooltip text={product.Zones[2]?.Name}>
+                    <img src={product.Zones[2]?.ImgUrl} alt={product.Zones[2]?.Name} dataTooltip={product.Zones[2]?.Name}/>
+                </Tooltip>
+                </>
+            );
+        } if (product.Zones?.length === 4) {
+            return (
+                <>
+                <Tooltip text={product.Zones[0]?.Name}>
+                    <img src={product.Zones[0]?.ImgUrl} alt={product.Zones[0]?.Name} dataTooltip={product.Zones[0]?.Name}/>
+                </Tooltip>
+                <Tooltip text={product.Zones[1]?.Name}>
+                    <img src={product.Zones[1]?.ImgUrl} alt={product.Zones[1]?.Name} dataTooltip={product.Zones[1]?.Name}/>
+                </Tooltip>
+                <Tooltip text={product.Zones[2]?.Name}>
+                    <img src={product.Zones[2]?.ImgUrl} alt={product.Zones[2]?.Name} dataTooltip={product.Zones[2]?.Name}/>
+                </Tooltip>
+                <Tooltip text={product.Zones[3]?.Name}>
+                    <img src={product.Zones[3]?.ImgUrl} alt={product.Zones[3]?.Name} dataTooltip={product.Zones[3]?.Name}/>
+                </Tooltip>
+                </>
+            );
+        }
+    }
+
 
     return (
         <>  
@@ -48,7 +103,10 @@ export default function ProductArticles() {
                         </div>           
                         <div className="article-details">
                             <h2 key={product.Keywords}>{product?.Name}</h2>
-                            <p>{product.MainCategory?.Name}</p>  
+                            <div className="details-section">
+                                <p>{product.MainCategory?.Name}</p>  
+                                <div className="badge-cntr">{getZones(product)}</div>
+                            </div>
                             <p className="description line-clamp">{product.Descriptions[0]?.Text}</p>
                         </div>
                     </article>
