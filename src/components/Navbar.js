05df166041f132d6290f5b9b63a88b8af2650848
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-05-31 11:03:10 +0200

    updt

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,56 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { NavLink } from "react-router-dom";
 import { FiHome, FiStar } from "react-icons/fi";
 
 export default function Navbar() {
-  const [isClosed, setClosed] = useState(true);
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    const open = document.querySelector(".burger");
+    const close = document.querySelector(".close");
+    const ul = document.querySelector("ul");
+
+    let tl3 = gsap.timeline({ defaults: { duration: 1, ease: "expo.inOut" } });
+    open.addEventListener("click", () => {
+      if (tl3.reversed()) {
+        tl3.play();
+      } else {
+        tl3
+          .to("nav", { right: 0 })
+          .to("nav", { height: "100vh" }, "-=.1")
+          .to("nav ul li a", { opacity: 1, pointerEvents: "all", stagger: 0.2 }, "-=.8")
+          .to(".close", { opacity: 1, pointerEvents: "all" }, "-=.8")
+          .to("nav h2", { opacity: 1 }, "-=1");
+      }
+    });
+
+    close.addEventListener("click", () => {
+      tl3.reverse();
+    });
+
+    ul.addEventListener("click", () => {
+      tl3.reverse();
+    });
+  }, []);
 
   return (
-    <nav>
-      <div>
-        <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
-          <span className="first-icon">Dark |</span>
-          <span className="sec-icon"> Light</span>
-        </NavLink>
+    <div className="navbarcontainer">
+ 
+      <div className="burger">
+        <div className="bars"></div>
       </div>
-
-      <div className={`burger ${isClosed ? "" : "open"}`} onClick={() => setClosed(!isClosed)}>
-        <div class="lines line1"></div>
-        <div class=" lines line2"></div>
-        <div class=" lines line3"></div>
-
-        <ul className={`menu ${isClosed ? "" : "open"}`}>
+      <nav>
+        <div className="close">
+          <div></div>
+        </div>
+        <ul>
           <li>
             <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
               <span className="icon">
                 Home
-                <FiHome />
               </span>
             </NavLink>
           </li>
@@ -32,20 +58,11 @@ export default function Navbar() {
             <NavLink to="/products" className={({ isActive }) => (isActive ? "active" : "")}>
               <span className="icon">
                 Produkter
-                <FiStar />
-              </span>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>
-              <span className="icon">
-                About
-                <FiStar />
               </span>
             </NavLink>
           </li>
         </ul>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
